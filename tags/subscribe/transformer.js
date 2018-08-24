813b2e21f16e9a1bbfabefa24ae004dd672afa00
Dylan Piercey <dpiercey@ebay.com>
2018-08-24 12:19:06 -0700

    feat(subscribe): remove tag from server compiled templates

--- tags/subscribe/transformer.js
+++ tags/subscribe/transformer.js
@@ -5,25 +5,29 @@ module.exports = function(el, ctx) {
   const { builder } = ctx;
   const events = [];
 
-  el.forEachAttribute(attr => {
-    if (attr.argument) {
-      let match;
-      let method;
-      let eventName;
+  if (ctx.isServerTarget()) {
+    el.detach();
+  } else {
+    el.forEachAttribute(attr => {
+      if (attr.argument) {
+        let match;
+        let method;
+        let eventName;
 
-      if ((match = onFormatDash.exec(attr.name))) {
-        method = match[1];
-        eventName = match[2];
-      } else if ((match = onFormatCamel.exec(attr.name))) {
-        method = match[1];
-        eventName = match[2].toLowerCase() + match[3];
+        if ((match = onFormatDash.exec(attr.name))) {
+          method = match[1];
+          eventName = match[2];
+        } else if ((match = onFormatCamel.exec(attr.name))) {
+          method = match[1];
+          eventName = match[2].toLowerCase() + match[3];
+        }
+
+        if (eventName) {
+          events.push(method, eventName);
+        }
       }
+    });
 
-      if (eventName) {
-        events.push(method, eventName);
-      }
-    }
-  });
-
-  el.setAttributeValue("__events", builder.literal(events));
+    el.setAttributeValue("__events", builder.literal(events));
+  }
 };
