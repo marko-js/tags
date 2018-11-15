ed75a2ef6c832f5fe142250f4aac1b0b46398277
Dylan Piercey <dpiercey@ebay.com>
2018-11-14 20:23:19 -0800

    chore(context): switch to using out instead of out.global for context storage

--- tags/context/src/helpers.js
+++ tags/context/src/helpers.js
@@ -2,31 +2,30 @@ var CONTEXT_KEY = "__subtree_context__";
 var HAS_BOUND_ASYNC_CONTEXT = "__bound_async_subtree_context__";
 
 exports.pushProvider = function(out, name, component, data) {
-  var outGlobal = out.global;
-
-  if (!outGlobal[HAS_BOUND_ASYNC_CONTEXT]) {
-    outGlobal[HAS_BOUND_ASYNC_CONTEXT] = true;
+  if (!out[HAS_BOUND_ASYNC_CONTEXT]) {
+    out[HAS_BOUND_ASYNC_CONTEXT] = true;
     out.on("beginAsync", bindSubtreeContextOnBeginAsync);
   }
 
-  var prevContext = outGlobal[CONTEXT_KEY];
-  var nextContext = (outGlobal[CONTEXT_KEY] = Object.create(prevContext || {}));
+  var prevContext = out[CONTEXT_KEY];
+  var nextContext = (out[CONTEXT_KEY] = Object.create(prevContext || {}));
   nextContext[name] = {
     data: data,
     component: component
   };
 
   return function popProvider() {
-    outGlobal[CONTEXT_KEY] = prevContext;
+    out[CONTEXT_KEY] = prevContext;
   };
 };
 
 exports.getProvider = function(out, name) {
-  return out.global[CONTEXT_KEY][name];
+  return out[CONTEXT_KEY][name];
 };
 
 function bindSubtreeContextOnBeginAsync(event) {
   var parent = event.parentOut;
   var child = event.out;
+  child[HAS_BOUND_ASYNC_CONTEXT] = true;
   child[CONTEXT_KEY] = parent[CONTEXT_KEY];
 }
