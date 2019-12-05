d7994ca75ff31b8ef3b23d15c31c3773f70e7979
Andrew Gliga <agliga@ebay.com>
2019-12-05 10:07:07 -0800

    feat: added state tag

---
+++ tags/state/README.md
@@ -0,0 +1,46 @@
+<h1 align="center">
+  <!-- Logo -->
+  <br/>
+  @marko-tags/state
+	<br/>
+
+  <!-- Stability -->
+  <a href="https://nodejs.org/api/documentation.html#documentation_stability_index">
+    <img src="https://img.shields.io/badge/stability-stable-green.svg" alt="API Stability"/>
+  </a>
+  <!-- NPM Version -->
+  <a href="https://npmjs.org/package/@marko-tags/state">
+    <img src="https://img.shields.io/npm/v/@marko-tags/state.svg" alt="NPM Version"/>
+  </a>
+  <!-- Downloads -->
+  <a href="https://npmjs.org/package/@marko-tags/state">
+    <img src="https://img.shields.io/npm/dm/@marko-tags/state.svg" alt="Downloads"/>
+  </a>
+</h1>
+
+Add state manipulation to your templates
+
+# Installation
+
+```console
+npm install @marko-tags/state
+```
+
+# Example
+
+```marko
+<state|myState = 1|>
+
+<div>
+  <div>${myState}</div>
+  <button onClick(() => myState++)>Increment</button>
+</div>
+```
+
+## API
+
+```marko
+<state|
+  stateAssignment
+/>
+```
---
+++ tags/state/test/test.browser.js
@@ -0,0 +1,117 @@
+const sinon = require("sinon");
+const { render, cleanup, fireEvent } = require("@marko/testing-library");
+const { expect, use } = require("chai");
+use(require("chai-dom"));
+use(require("sinon-chai"));
+
+const EventEmitter = require("events").EventEmitter;
+const simpleState = require("./fixtures/simple");
+const assignmentState = require("./fixtures/assignment");
+const booleanState = require("./fixtures/boolean");
+const multiState = require("./fixtures/multi");
+const largeFunction = require("./fixtures/largeFunction");
+const noInit = require("./fixtures/noInitializer");
+
+describe("browser", () => {
+  afterEach(cleanup);
+
+  describe("State update", () => {
+    it("Updates simple state", async () => {
+      const { rerender, getByText } = await render(simpleState, {
+        value: 1
+      });
+      const button = getByText("Increment");
+
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("2").tagName).to.equal("DIV");
+
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("3").tagName).to.equal("DIV");
+    });
+
+    it("Updates multi state", async () => {
+      const { rerender, getByText } = await render(multiState, {
+        value: 3,
+        value2: 10
+      });
+      const button = getByText("Increment");
+
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("4").id).to.equal("1");
+      expect(getByText("9").id).to.equal("2");
+
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("5").id).to.equal("1");
+      expect(getByText("8").id).to.equal("2");
+    });
+
+    it("Updates boolean state", async () => {
+      const { rerender, getByText } = await render(booleanState);
+      const button = getByText("Toggle");
+
+      expect(getByText("false").tagName).to.equal("DIV");
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("true").tagName).to.equal("DIV");
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("false").tagName).to.equal("DIV");
+    });
+
+    it("Updates assignment state", async () => {
+      const { rerender, getByText } = await render(assignmentState);
+      const button = getByText("Switch");
+
+      expect(getByText("1").tagName).to.equal("DIV");
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("2").tagName).to.equal("DIV");
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("1").tagName).to.equal("DIV");
+    });
+
+    it("Updates large function properly", async () => {
+      const { rerender, getByText, getAllByText } = await render(largeFunction);
+      const button = getByText("RunMe");
+
+      expect(getByText("1").id).to.equal("1");
+      expect(getAllByText("2").length).to.equal(2);
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("3").id).to.equal("1");
+      expect(getByText("2").id).to.equal("2");
+      expect(getByText("-1").id).to.equal("3");
+
+      fireEvent.click(button);
+      await rerender();
+      expect(getAllByText("3").length).to.equal(2);
+      expect(getByText("-1").id).to.equal("3");
+
+      fireEvent.click(button);
+      await rerender();
+      expect(getAllByText("1").length).to.equal(2);
+      expect(getByText("3").id).to.equal("2");
+    });
+
+    it("Updates with no initializer properly", async () => {
+      const { rerender, getByText, getAllByText } = await render(noInit);
+      const button = getByText("Increment");
+
+      expect(getByText("1").id).to.equal("1");
+      expect(getByText("notInit").id).to.equal("2");
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("2").id).to.equal("1");
+      expect(getByText("1").id).to.equal("2");
+      fireEvent.click(button);
+      await rerender();
+      expect(getByText("3").id).to.equal("1");
+      expect(getByText("2").id).to.equal("2");
+    });
+  });
+});
