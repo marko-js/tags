a79321a8073abe0e5c486e574803c763d70b38e0
Dylan Piercey <dpiercey@ebay.com>
2020-01-08 13:48:56 -0700

    fix(portal): improve logic for moving target, support older browsers (#19)

---
+++ tags/portal/test/test.server.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const sinon = require("sinon");
+const { render } = require("@marko/testing-library");
+const template = require("../");
+
+describe("server", () => {
+  it("does not render anything", async () => {
+    const renderBodySpy = sinon.spy();
+    const { container } = await render(template, {
+      renderBody: renderBodySpy,
+      target: "somewhere"
+    });
+
+    assert.ok(renderBodySpy.notCalled);
+    assert.strictEqual(container.childElementCount, 0);
+  });
+});
